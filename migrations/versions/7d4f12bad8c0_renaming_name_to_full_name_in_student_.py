d681eb275593ff1e40237bd9907b5790d1d22454
VictorIgati <victor.igati@student.moringaschool.com>
2025-03-03 12:32:54 +0300

    refactor: rename 'name' to 'full_name' in Student model and update migration files

---
+++ migrations/versions/7d4f12bad8c0_renaming_name_to_full_name_in_student_.py
@@ -0,0 +1,24 @@
+"""Renaming name to full_name in Student model
+
+Revision ID: 7d4f12bad8c0
+Revises: 
+Create Date: 2025-03-03 12:27:31.941476
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7d4f12bad8c0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
